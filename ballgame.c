404ac289bdb5c6ace9920f29c947d35280fb5b7b
Harish Babu <160093297+ooowll@users.noreply.github.com>
2025-03-18 15:19:19 -0400

    Made crosshair and added double buffering

--- ballgame.c
+++ ballgame.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>	
 #define FPGA_PIXEL_BUF_BASE 0x08000000
 #define LEDR_BASE 0xFF200000
 #define HEX3_HEX0_BASE 0xFF200020
@@ -14,8 +17,13 @@
 #include <stdint.h>
 	
 int pixel_buffer_start;
+short int Buffer1[240][512]; // 240 rows, 512 (320 + padding) columns
+short int Buffer2[240][512];
+
 int cursorx = 160;
 int cursory = 120; 
+int prev_cursorx = 160;
+int prev_cursory = 120;
 bool led_array[10] = {0}; 
 int direction_array[4] = {0};
 char b1 = 0, b2 = 0, b3 = 0;
@@ -30,18 +38,28 @@ char keyboard_movement(char b1, char b2);
 
 int main(void) {
     volatile int *LEDR = LEDR_BASE;
-    volatile int *pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
 	volatile long int *clock = (long int *)CLOCK_BASE;
-    pixel_buffer_start = *pixel_ctrl_ptr; //Read location of the pixel buffer from the pixel buffer controller
-    clear_screen();
+    volatile int *pixel_ctrl_ptr = (int *)PIXEL_BUF_CTRL_BASE;
+   /* set front pixel buffer to Buffer 1 */
+    *(pixel_ctrl_ptr + 1) = (int) &Buffer1; // first store the address in the  back buffer
+    /* now, swap the front/back buffers, to set the front buffer location */
+    wait_for_vsync();
+    /* initialize a pointer to the pixel buffer, used by drawing functions */
+    pixel_buffer_start = *pixel_ctrl_ptr;
+    clear_screen(); // pixel_buffer_start points to the pixel buffer
+    /* set back pixel buffer to Buffer 2 */
+    *(pixel_ctrl_ptr + 1) = (int) &Buffer2;
+    pixel_buffer_start = *(pixel_ctrl_ptr + 1); // we draw on the back buffer
+    clear_screen(); // pixel_buffer_start points to the pixel buffer
     while (1) {
+		wait_for_vsync(); // swap front and back buffers on VGA vertical sync
+		pixel_buffer_start = *(pixel_ctrl_ptr + 1); // new back buffer
+		plot_crosshair(prev_cursorx, prev_cursory, 0);
+		prev_cursorx = cursorx;
+		prev_cursory = cursory;
 		update_cursor();
-		*LEDR = led_array[9] << 9 | led_array[8] << 8 | led_array[7] << 7 | led_array[6] << 6 | led_array[5] << 5 | led_array[4] << 4 | led_array[3] << 3 | led_array[2] << 2 | led_array[1] << 1 | led_array[0] << 0;	
-		plot_pixel(cursorx, cursory, 0xFFFF);
-		if((*clock % 1000)==0){
-		cursorx = cursorx+direction_array[0]+direction_array[3];
-		cursory = cursory+direction_array[1]+direction_array[2];
-		}
+		plot_crosshair(cursorx, cursory, 0xFFFF);
+        
     }
 }
 void HEX_PS2(char b1, char b2, char b3) {
@@ -81,21 +99,35 @@ void clear_screen(){
 void plot_pixel(int x, int y, short int line_color)
 {
     volatile short int *one_pixel_address;
-	if(x >= 319){
-	x = 319;	
-	}
-	if(x <= 0){
-	x = 0;	
-	}
-	if(y >= 239){
-	y = 239;	
-	}
-	if(y <= 0){
-	y = 0;	
-	}
     one_pixel_address =  pixel_buffer_start + (y << 10) + (x << 1);
     *one_pixel_address = line_color;
 }
+void plot_crosshair(int x, int y, short int line_color)
+{
+    volatile short int *one_pixel_address;
+	if(x+2 >= 319){
+	x = 319-2;	
+	}
+	if(x-2 <= 0){
+	x = 2;	
+	}
+	if(y+2 >= 239){
+	y = 239-2;	
+	}
+	if(y-2 <= 0){
+	y = 0+2;	
+	}
+	plot_pixel(x, y, line_color);
+	plot_pixel(x+1, y, line_color);
+	plot_pixel(x+2, y, line_color);
+	plot_pixel(x-1, y, line_color);
+	plot_pixel(x-2, y, line_color);
+	plot_pixel(x, y+1, line_color);
+	plot_pixel(x, y+2, line_color);
+	plot_pixel(x, y-1, line_color);
+	plot_pixel(x, y-2, line_color);
+}
+
 void wait_for_vsync()
 {
 	volatile int * pixel_ctrl_ptr = (int *) 0xff203020; // base address
@@ -157,8 +189,6 @@ void update_cursor(void){
 			direction_array[2] = -1;
 		}
 	} 
-	led_array[0] = direction_array[0] == 1;
-	led_array[1] = direction_array[1] == 1;
-	led_array[2] = direction_array[2] == -1;
-	led_array[3] = direction_array[3] == -1;
+		cursorx = cursorx+direction_array[0]+direction_array[3];
+		cursory = cursory+direction_array[1]+direction_array[2];
 }
